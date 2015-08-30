c005695e250da0bd94fd855231356b1f856c27d4
Jonathan Dushoff <dushoff@mcmaster.ca>
2015-08-30 12:27:29 -0400

    Autocommit On branch master Your branch is up-to-date with 'origin/master'.
    Changes to be committed:
      (use "git reset HEAD <file>..." to unstage)
    
    	modified:   oldlatex.mk
    
    Untracked files:
      (use "git add <file>..." to include in what will be committed)
    
    	.Makefile.swp
    	.oldlatex.mk.swp
    	commit.time
    	gitcomment.txt
    	linux.mk
    	local.mk

--- oldlatex.mk
+++ oldlatex.mk
@@ -1,25 +1,21 @@
 latex = pdflatex
 
-# dvi is based on both .tex and .aux, and does complicated stuff, to try
-# to figure out when it's done (and sometimes run twice with one make).
-
-%.pdf: %.tex
-
 %.ltx:
+	/bin/cp -f $*.aux .$*.aux
 	$(latex) $*; sleep 1
-	diff tmp.aux $*.aux > /dev/null || touch $@
+	diff .$*.aux $*.aux > /dev/null || touch $*.aux
 
 %.aux: %.tex 
 	touch $@
-	/bin/cp -f $@ tmp.aux
 	$(MAKE) $*.ltx
 
-%.pdf: %.aux 
-	diff tmp.aux $@ > /dev/null || $(MAKE) $*.ltx
-
-# Make .pdf depend on .bbl when appropriate
+%.pdf: %.tex
+	touch $<
+	$(MAKE) $*.aux
+	diff .$< $< > /dev/null || $(MAKE) $*.ltx
 
 %.bbl: %.aux 
+	/bin/rm -f $@
 	bibtex $*
 
 tclean:
