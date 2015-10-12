f8c0e97afb29c404224982e41056312fd639fc3d
Jonathan Dushoff <dushoff@mcmaster.ca>
2015-10-12 18:21:00 -0400

    %.makestuff, *.r On branch master Your branch is up-to-date with 'origin/master'.
    Changes to be committed:
      (use "git reset HEAD <file>..." to unstage)
    
    	modified:   .gitignore
    	modified:   git.mk
    	modified:   makefile.mk

--- makefile.mk
+++ makefile.mk
@@ -10,6 +10,20 @@ ms = ../makestuff
 
 ##################################################################
 
+### Makestuff
+
+## Change this name to download a new version of the makestuff directory
+Makefile: start.makestuff
+
+msrepo = https://github.com/dushoff
+
+%.makestuff:
+	-cd $(dir $(ms)) && mv -f $(notdir $(ms)) .$(notdir $(ms))
+	cd $(dir $(ms)) && git clone $(msrepo)/$(notdir $(ms)).git
+	cd $(dir $(ms)) && git clone $(msrepo)/$(notdir $(ms)).git
+	-cd $(dir $(ms)) && rm -rf $(ms) .$(notdir $(ms))
+	touch $@
+
 -include $(ms)/local.mk
 -include local.mk
 -include $(ms)/git.mk
