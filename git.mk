1f72ea9dccb19d2e93a30664b2266e8953eeaff2
Jonathan Dushoff <dushoff@mcmaster.ca>
2015-12-19 07:43:14 -0500

    Autocommit (makestuff) On branch master Your branch is ahead of 'origin/master' by 1 commit.   (use "git push" to publish your local commits)
    Changes to be committed:
      (use "git reset HEAD <file>..." to unstage)
    
    	modified:   git.mk
    	modified:   linux.mk
    
    Untracked files:
      (use "git add <file>..." to include in what will be committed)
    
    	talk/talk

--- git.mk
+++ git.mk
@@ -33,8 +33,8 @@ psync:
 	$(MAKE) pull
 	$(MAKE) push
 
-commit.txt: $(Sources)
-	git add -f $^ $(Archive)
+commit.txt: $(Sources) $(Archive)
+	git add -f $^
 	echo "Autocommit ($(notdir $(CURDIR)))" > $@
 	-git commit --dry-run >> $@
 	gvim -f $@
--- linux.mk
+++ linux.mk
@@ -16,6 +16,7 @@ CPF = /bin/cp -f
 DIFF = diff
 EDIT = gvim
 RMR = /bin/rm -rf
+LN = /bin/ln -s
 
 copy = $(CP) $< $@
 ccrib = $(CP) $(crib)/$@ .
