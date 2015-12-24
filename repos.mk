de407ecec5c077adcec85dee966d0ae34cd29adb
Jonathan Dushoff <dushoff@mcmaster.ca>
2015-12-23 23:15:23 -0500

    Fixed repos.mk?

--- repos.mk
+++ repos.mk
@@ -1,8 +1,5 @@
 dushoff_github = https://github.com/dushoff
-theobio_github_github = https://github.com/theobio
-
-$(ms)/talk:
-	cd $(dir $($@)) && git clone $(dushoff_github)/$(notdir $($@)).git
+theobio_github = https://github.com/mac-theobio
 
 $(gitroot)/Disease_data:
-	cd $(dir $($@)) && git clone $(theobio_github)/$(notdir $($@)).git
+	cd $(dir $@) && git clone $(theobio_github)/$(notdir $@).git
