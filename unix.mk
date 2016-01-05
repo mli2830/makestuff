a1f217a5c71177c52bd1c46d4c3b539056b503dd
Jonathan Dushoff <dushoff@mcmaster.ca>
2016-01-04 22:59:36 -0500

    Autocommit (makestuff) On branch master Your branch is up-to-date with 'origin/master'.
    Changes to be committed:
      (use "git reset HEAD <file>..." to unstage)
    
    	modified:   lect/lect.format
    	modified:   unix.mk
    
    Untracked files:
      (use "git add <file>..." to include in what will be committed)
    
    	commit.time

--- unix.mk
+++ unix.mk
@@ -1,3 +1,4 @@
+# Unix
 MVF = /bin/mv -f
 MV = /bin/mv
 CP = /bin/cp
@@ -6,8 +7,17 @@ DIFF = diff
 EDIT = gvim
 RMR = /bin/rm -rf
 LN = /bin/ln -s
+TGZ = tar czf $@ $^
+MD = mkdir
+CAT = cat
 
+# Generic
+link = $(LN) $< $@
 copy = $(CP) $< $@
 ccrib = $(CP) $(crib)/$@ .
+mkdir = $(MD) $@
+cat = $(CAT) $^ > $@
+
+# What?
 convert = convert $< $@
 shell_execute = sh < $@
